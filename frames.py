d9ac4738a445e20b87e5b6527738a631e84928ed
Jesus Medina <chuymedina96@gmail.com>
2020-05-21 21:07:36 -0500

    more

--- frames.py
+++ frames.py
@@ -12,32 +12,26 @@ def process_img(original_image):
     processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
     return processed_img
 
-for i in list(range(4)) [::-1]:
-    print(i+1)
-    time.sleep(1)
+def main():
+    last_time = time.time()
 
+    while(True):
+        screen = np.array(ImageGrab.grab(bbox=(0,40, 800, 640)))
+        new_screen = process_img(screen)
+        print('down')
+        PressKey(W)
+        time.sleep(3)
+        print('up')
+        ReleaseKey(W)
+        # printscreen_numpy = np.array(printscreen_pil.getdata(), dtype='uint8') \
+        # .reshape((printscreen_pil.size[1], printscreen_pil.size[0], 3))
 
-last_time = time.time()
+        print('Loop took {} seconds'.format(time.time()-last_time))
+        last_time=time.time()
 
-while(True):
-    screen = np.array(ImageGrab.grab(bbox=(0,40, 800, 640)))
-    new_screen = process_img(screen)
-    print('down')
-    PressKey(W)
-    time.sleep(3)
-    print('up')
-    ReleaseKey(W)
-    # printscreen_numpy = np.array(printscreen_pil.getdata(), dtype='uint8') \
-    # .reshape((printscreen_pil.size[1], printscreen_pil.size[0], 3))
+        cv2.imshow('window', new_screen)
 
-
-
-    print('Loop took {} seconds'.format(time.time()-last_time))
-    last_time=time.time()
-
-    cv2.imshow('window', new_screen)
-
-    # cv2.imshow('window', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break
+        # cv2.imshow('window', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
